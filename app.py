748e67ae7eaffd21ce3ae7a0a0b14a31ec6e9f53
shyam kumar S <23pds027@loyolacollege.edu>
2024-08-27 17:25:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -35,4 +35,4 @@ def index():
     return render_template('index.html', prediction=prediction)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
